6691eac84f56779e08c000438219eea1b1ec423c
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-05-07 19:52:28 +0530

    on 7th may

---
+++ j6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class j6 {
+    public static void printMyName(String name){
+        System.out.println("The name is : "+ name);
+    }
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the name :");
+    String name = sc.nextLine();
+    printMyName(name);
+
+        sc.close();
+    }
+}
