0da342008375559daee2a62f2a7e51b08757d0e3
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-05-17 20:02:27 +0530

    timecomplex on 17th may

---
+++ timecomplex.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class timecomplex {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+
+       for (int i = 0; i < n; i++) {
+        System.out.println("hello");
+       }
+
+       for (int j = 0; j < m ;j++){
+        System.out.println("Hello");}
+    sc.close();
+    } 
+
+    
+}
