863067ff5197a396c86b0f1a7434eacfdecffbcd
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-07-14 20:53:39 +0530

    updating the workspace until 14th july

--- string.java
+++ string.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class string {
     public static void main(String[] args) {
-        String name = "Pinaki";
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Your name is:"+name);
+
+        String fullName = sc.nextLine();
+        System.out.println("Your full name is :"+fullName)  ;
+
+
+
+
+        sc.close();
     }
 }
