f071ef764bb9166b5a8d4bb35b964981cce0c6c0
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-09-04 21:14:03 +0530

    starting the dsa again

--- array/a1.java
+++ array/a1.java
@@ -1,8 +1,17 @@
-package array;
-
 public class a1 {
     public static void main(String[] args) {
-        
-        
-    }  
+        int[] marks = new int[3];
+        marks[0] = 100;
+        marks[1] = 90 ;
+        marks[2] = 80 ;
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+
+
+        for(int i = 0 ; i < marks.length;i++){
+            System.out.println(marks[i]);
+        }
+}
+
 }
