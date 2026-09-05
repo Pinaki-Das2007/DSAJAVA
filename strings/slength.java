021cdba0cee1972e8eab69f574b6949d677ca262
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-09-05 21:46:58 +0530

    length() used

---
+++ strings/slength.java
@@ -0,0 +1,6 @@
+public class slength {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println("Length of the string is:" + str.length());
+    }
+}
