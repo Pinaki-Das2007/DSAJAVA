236cd8bb0c0bec65b68c2fe6a3b501476f717bac
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-09-05 21:50:49 +0530

    charAt() function completed

---
+++ strings/scharat.java
@@ -0,0 +1,12 @@
+public class scharat {
+    public static void main(String[] args) {
+        String str = "Hello world";
+        System.out.println("char at index 4 is " + str.charAt(4));
+
+        int i = 0;
+        String newline = "My name is Harry Potter";
+        for(i = 0; i < newline.length(); i ++){
+            System.out.println(newline.charAt(i));
+        }
+    }
+}
