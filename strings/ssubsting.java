81f85bd1b81623191d0dcc52a3f1a4e761547fbf
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-09-06 11:54:28 +0530

    substring part completed

---
+++ strings/ssubsting.java
@@ -0,0 +1,7 @@
+public class ssubsting {
+    public static void main(String[] args) {
+        String sentence = "My name is Harry";
+        String name = sentence.substring(11,sentence.length());
+        System.out.println("The name is " + name);
+    }
+}
