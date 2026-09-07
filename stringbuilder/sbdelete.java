55c5af89e2c4a8b9235fd37d1d123052237f0977
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-09-07 21:08:53 +0530

    delete function completed

---
+++ stringbuilder/sbdelete.java
@@ -0,0 +1,9 @@
+public class sbdelete {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Tonny");
+        System.out.println(sb);
+
+        sb.delete(2, 3);
+        System.out.println(sb);
+    }
+}
